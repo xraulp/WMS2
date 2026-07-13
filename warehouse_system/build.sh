75cac1bdf3d1d7f1e3c04120b987c3f0dc45c398
xraulp <rauldelunaxp@gmail.com>
2026-07-13 13:28:56 -0500

    Update build.sh

--- warehouse_system/build.sh
+++ warehouse_system/build.sh
@@ -1,1 +1,11 @@
+#!/usr/bin/env bash
+# build.sh - Script de construcción para Render
 
+echo "-----> Installing Python dependencies..."
+pip install -r requirements.txt
+
+echo "-----> Running Django migrations..."
+python manage.py migrate
+
+echo "-----> Collecting static files..."
+python manage.py collectstatic --noinput
