5988eb3fedde88c646dc3d74002ff0218266d6fd
xraulp <rauldelunaxp@gmail.com>
2026-07-18 10:22:23 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,5 +7,8 @@ pip install -r requirements.txt
 echo "-----> Running Django migrations..."
 python manage.py migrate
 
+echo "-----> Loading initial data..."          # ← Agrega esto
+python manage.py loaddata data.json            # ← Agrega esto
+
 echo "-----> Collecting static files..."
 python manage.py collectstatic --noinput
