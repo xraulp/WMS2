beac5d5dfa531a4f5b37500dc3a402b26445cf30
xraulp <208318956+xraulp@users.noreply.github.com>
2026-07-19 20:10:14 -0700

    fix: Actualiza build.sh

--- warehouse_system/build.sh
+++ warehouse_system/build.sh
@@ -1,11 +1,22 @@
 #!/usr/bin/env bash
 # build.sh - Script de construcción para Render
 
+#!/usr/bin/env bash
+
 echo "-----> Installing Python dependencies..."
 pip install -r requirements.txt
 
+echo "-----> Exporting DATABASE_URL..."
+export DATABASE_URL="${DATABASE_URL}"   # <--- Agrega esta línea
+
 echo "-----> Running Django migrations..."
 python manage.py migrate
+
+echo "-----> Loading initial data..."
+python manage.py loaddata data.json
+
+echo "-----> Creating superuser if not exists..."
+python create_superuser.py
 
 echo "-----> Collecting static files..."
 python manage.py collectstatic --noinput
